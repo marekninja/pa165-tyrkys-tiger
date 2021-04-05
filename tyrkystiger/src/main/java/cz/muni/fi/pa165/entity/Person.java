72523fadb2be332f6d15cb9c97d65837123e9108
Peter Mravec <peter.mravec@sap.com>
2021-04-05 23:16:35 +0200

    added missing getter and setter for id

--- tyrkystiger/src/main/java/cz/muni/fi/pa165/entity/Person.java
+++ tyrkystiger/src/main/java/cz/muni/fi/pa165/entity/Person.java
@@ -29,6 +29,14 @@ public class Person {
         this.id = id;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
