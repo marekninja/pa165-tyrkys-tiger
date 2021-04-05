76fa73c18716988e29efdabe666664b1b27d57da
Matej Turek <469107@fi.muni.cz>
2021-04-05 13:17:08 +0200

    [ADD] Enum Genre for entity Movie created.

--- tyrkystiger/src/main/java/cz/muni/fi/pa165/enums/Genre.java
+++ tyrkystiger/src/main/java/cz/muni/fi/pa165/enums/Genre.java
@@ -1,2 +1,30 @@
-package cz.muni.fi.pa165.enums;public enum Genre {
+package cz.muni.fi.pa165.enums;
+
+/**
+ * This enum contains all basic movie categories.
+ *
+ * @author Matej Turek
+ */
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    ANIME,
+    AWARDWINNING,
+    FAMILY,
+    CLASSIC,
+    COMEDY,
+    CRIME,
+    DOCUMENTARY,
+    DRAMA,
+    FANTASY,
+    HISTORY,
+    HOLLYWOOD,
+    HORROR,
+    INDEPENDENT,
+    MUSIC_MUSICALS,
+    ROMANCE,
+    SCIFI,
+    SPORT,
+    STANDUPCOMEDY,
+    THRILLER
 }
