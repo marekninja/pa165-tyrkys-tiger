ec79351a9bcb40742ae262a09cb7acf8bb2244a1
Peter Mravec <peter.mravec@sap.com>
2021-04-06 15:02:17 +0200

    Added PersonDao and PersonDaoImpl

---
+++ tyrkystiger/src/main/java/cz/muni/fi/pa165/dao/PersonDao.java
@@ -0,0 +1,58 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.Person;
+
+import java.util.List;
+
+/**
+ * Dao interface for entity Person
+ * Supports basic CRUD operations.
+ *
+ * @author Peter Mravec
+ */
+public interface PersonDao {
+
+    /**
+     * Finds Person by id
+     *
+     * @param id of Person
+     * @return found Person
+     */
+    Person findById(Long id);
+
+    /**
+     * Finds Person by his name
+     *
+     * @param name of Person
+     * @return found Person
+     */
+    Person findByName(String name);
+
+    /**
+     * Finds all Persons
+     *
+     * @return list of all Persons
+     */
+    List<Person> findAll();
+
+    /**
+     * Inserts Peron into DB
+     *
+     * @param person Person object to create
+     */
+    Long createPerson(Person person);
+
+    /**
+     * Updates Person in DB
+     *
+     * @param person Person object to update
+     */
+    void updatePerson(Person person);
+
+    /**
+     * Deletes Person from DB
+     *
+     * @param person Person object to delete
+     */
+    void deletePerson(Person person);
+}
