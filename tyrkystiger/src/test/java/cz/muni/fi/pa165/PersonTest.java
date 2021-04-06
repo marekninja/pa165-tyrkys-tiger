f93ce8d09fb0f8d3956e557f0316f1cf171897fa
Matej Turek <469107@fi.muni.cz>
2021-04-06 19:34:38 +0200

    [ADD] Init classes for unit tests of Person and UserRating.

--- tyrkystiger/src/test/java/cz/muni/fi/pa165/PersonTest.java
+++ tyrkystiger/src/test/java/cz/muni/fi/pa165/PersonTest.java
@@ -1,4 +1,30 @@
-package cz.muni.fi.pa165;/**
+package cz.muni.fi.pa165;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+/**
+ * Unit tests for class Person
+ *
  * @author Matej Turek
- */public class PersonTest {
+ */
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+public class PersonTest extends AbstractTestNGSpringContextTests {
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    Person person;
+
+    @BeforeClass
+    public void init() {
+        person = new Person();
+
+
+    }
 }
--- tyrkystiger/src/test/java/cz/muni/fi/pa165/UserRatingTest.java
+++ tyrkystiger/src/test/java/cz/muni/fi/pa165/UserRatingTest.java
@@ -1,4 +1,33 @@
-package cz.muni.fi.pa165;/**
+package cz.muni.fi.pa165;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+/**
+ * Unit tests for entity UserRating
+ *
  * @author Matej Turek
- */public class UserRatingTest {
+ */
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+public class UserRatingTest extends AbstractTestNGSpringContextTests {
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    UserRating userRating;
+
+    @BeforeClass
+    public void init() {
+        userRating = new UserRating();
+    }
+
+    @Test
+    public void Test() {
+
+    }
 }
