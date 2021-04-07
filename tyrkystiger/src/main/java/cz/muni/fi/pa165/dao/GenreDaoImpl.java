9bf6593a09e987be4ce8b9610ad997a46667e730
Matej Turek <469107@fi.muni.cz>
2021-04-07 20:39:48 +0200

    [ADD] Dao and daoImpl for Genre created.

---
+++ tyrkystiger/src/main/java/cz/muni/fi/pa165/dao/GenreDaoImpl.java
@@ -0,0 +1,44 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.Genre;
+import cz.muni.fi.pa165.entity.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * This is a base implementation of {@link GenreDao}
+ *
+ * @author Matej Turek
+ */
+public class GenreDaoImpl implements GenreDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Genre findById(Long id) {
+        return em.find(Genre.class, id);
+    }
+
+    @Override
+    public List<Genre> findAll() {
+        return em.createQuery("select u from User u", User.class).getResultList()
+    }
+
+    @Override
+    public void createGenre(Genre genre) {
+        em.persist(genre);
+    }
+
+    @Override
+    public Genre updateGenre(Genre genre) {
+        return em.merge(genre);
+    }
+
+    @Override
+    public void deleteUser(Genre genre) {
+        em.remove(genre);
+    }
+}
