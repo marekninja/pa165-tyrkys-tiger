431868dad74b751211c0906908da351f578f8e1e
Marek Petrovič <32146793+marekninja@users.noreply.github.com>
2021-04-22 16:21:54 +0200

    [FIX] Movie yearMade PastOrPresent anotated, was only Past

--- recommender-persistence/src/main/java/cz/muni/fi/pa165/entity/Movie.java
+++ recommender-persistence/src/main/java/cz/muni/fi/pa165/entity/Movie.java
@@ -6,10 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -46,7 +43,7 @@ public class Movie {
     @OneToMany(mappedBy = "movieGallery")
     private Set<Image> gallery = new HashSet<>();
 
-    @Past
+    @PastOrPresent
     private LocalDate yearMade;
 
     //    maybe make enum for countries
