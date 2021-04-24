10dc061c213bdcc24c94d441e347efc71562c1d3
Peter Mravec <peter.mravec@sap.com>
2021-04-24 15:21:09 +0200

    UserRating uses Lombok, removed old getXX() and setXX() methods

--- recommender-persistence/src/main/java/cz/muni/fi/pa165/entity/UserRating.java
+++ recommender-persistence/src/main/java/cz/muni/fi/pa165/entity/UserRating.java
@@ -1,5 +1,10 @@
 package cz.muni.fi.pa165.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -13,6 +18,10 @@ import java.util.Objects;
  */
 @Entity
 @Table(name = "userRatings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRating {
 
     @Id
@@ -50,82 +59,6 @@ public class UserRating {
     @Min(0)
     @Max(10)
     private Integer overallScore;
-
-    public UserRating() {}
-
-    public UserRating(long id) {
-        this.id = id;
-    }
-
-    public UserRating(@NotNull Movie movie, @NotNull User user) {
-        this.movie = movie;
-        this.user = user;
-    }
-
-    public UserRating(@NotNull Movie movie, @NotNull User user, @NotNull @Min(0) @Max(10) Integer storyScore, @NotNull @Min(0) @Max(10) Integer visualScore, @NotNull @Min(0) @Max(10) Integer actorScore, @NotNull @Min(0) @Max(10) Integer overallScore) {
-        this.movie = movie;
-        this.user = user;
-        this.storyScore = storyScore;
-        this.visualScore = visualScore;
-        this.actorScore = actorScore;
-        this.overallScore = overallScore;
-    }
-
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public void setMovie(Movie movie) {
-        this.movie = movie;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getStoryScore() {
-        return storyScore;
-    }
-
-    public void setStoryScore(Integer storyScore) {
-        this.storyScore = storyScore;
-    }
-
-    public Integer getVisualScore() {
-        return visualScore;
-    }
-
-    public void setVisualScore(Integer visualScore) {
-        this.visualScore = visualScore;
-    }
-
-    public Integer getActorScore() {
-        return actorScore;
-    }
-
-    public void setActorScore(Integer actorScore) {
-        this.actorScore = actorScore;
-    }
-
-    public Integer getOverallScore() {
-        return overallScore;
-    }
-
-    public void setOverallScore(Integer overallScore) {
-        this.overallScore = overallScore;
-    }
 
     @Override
     public boolean equals(Object o) {
