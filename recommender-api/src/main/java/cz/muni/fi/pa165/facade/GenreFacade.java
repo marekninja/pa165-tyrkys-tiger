7a88edc208c3ab30afef40ae37c96daa91bb7131
Peter Mravec <peter.mravec@sap.com>
2021-04-27 22:02:54 +0200

    Create GenreFacade in recommender-api

---
+++ recommender-api/src/main/java/cz/muni/fi/pa165/facade/GenreFacade.java
@@ -0,0 +1,47 @@
+package cz.muni.fi.pa165.facade;
+
+import cz.muni.fi.pa165.dto.GenreDTO;
+import java.util.List;
+
+/**
+ * @author Peter Mravec
+ */
+public interface GenreFacade {
+
+    /**
+     * Creates Genre by GenreDTO
+     *
+     * @param genreDTO of GenreDTO
+     */
+    void createGenre(GenreDTO genreDTO);
+
+    /**
+     * Finds Genre by id
+     *
+     * @param id of Genre
+     * @return found Genre
+     */
+    GenreDTO findGenreById(Long id);
+
+    /**
+     * Finds all Genres
+     *
+     * @return list of all Genres
+     */
+    List<GenreDTO> findAllGenres();
+
+    /**
+     * Updates existing Genre
+     *
+     * @param genreDTO Genre object to update
+     * @return updated GenreDTO object
+     */
+    GenreDTO updateGenre(GenreDTO genreDTO);
+
+    /**
+     * Deletes existing Genre
+     *
+     * @param id Id of the Genre object that should be deleted
+     */
+    void deleteGenre(Long id);
+}
