f95ab87d806344cf7fd5f711203fbeb5be303e54
Matej Turek <469107@fi.muni.cz>
2021-04-27 16:35:21 +0200

    [ADD] Validator and null_exception added

--- recommender-service/src/main/java/cz/muni/fi/pa165/service/utils/Validator.java
+++ recommender-service/src/main/java/cz/muni/fi/pa165/service/utils/Validator.java
@@ -1,4 +1,15 @@
-package cz.muni.fi.pa165.service.utils;/**
+package cz.muni.fi.pa165.service.utils;
+
+import cz.muni.fi.pa165.service.exceptions.NullArgumentException;
+
+/**
  * @author Matej Turek
- */public class Validator {
+ */
+public class Validator {
+
+    public static void validate(Class<?> clazz, Object object, String message) {
+        if (object == null) {
+            throw new NullArgumentException(clazz, message);
+        }
+    }
 }
