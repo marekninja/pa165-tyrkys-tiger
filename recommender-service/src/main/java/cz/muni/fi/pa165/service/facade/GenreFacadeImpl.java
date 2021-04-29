0c9518d645760c85a66f64f376c196895551acfa
Peter Mravec <peter.mravec@sap.com>
2021-04-29 22:07:54 +0200

    Created GenreFacadeImpl and PersonFacadeImpl

---
+++ recommender-service/src/main/java/cz/muni/fi/pa165/service/facade/GenreFacadeImpl.java
@@ -0,0 +1,62 @@
+package cz.muni.fi.pa165.service.facade;
+
+import cz.muni.fi.pa165.dto.GenreDTO;
+import cz.muni.fi.pa165.entity.Genre;
+import cz.muni.fi.pa165.facade.GenreFacade;
+import cz.muni.fi.pa165.service.BeanMappingService;
+import cz.muni.fi.pa165.service.GenreService;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * @author Peter Mravec
+ */
+@Service
+@Transactional
+public class GenreFacadeImpl implements GenreFacade {
+
+    private final GenreService genreService;
+
+    private final BeanMappingService beanMappingService;
+
+    @Inject
+    public GenreFacadeImpl(GenreService genreService, BeanMappingService mapping) {
+        this.genreService = genreService;
+        this.beanMappingService = mapping;
+    }
+
+    @Override
+    public void createGenre(GenreDTO genreDTO) {
+        Genre genre = new Genre();
+        genre.setName(genreDTO.getName());
+        genreService.createGenre(genre);
+    }
+
+    @Override
+    public GenreDTO findGenreById(Long id) {
+        Genre storedGenre = genreService.findGenreById(id);
+        return (storedGenre == null) ? null :
+                beanMappingService.mapTo(storedGenre, GenreDTO.class);
+    }
+
+    @Override
+    public List<GenreDTO> findAllGenres() {
+        return beanMappingService.mapTo(genreService.findAllGenres(), GenreDTO.class);
+    }
+
+    @Override
+    public GenreDTO updateGenre(GenreDTO genreDTO) {
+        Genre updated = genreService.updateGenre(beanMappingService.mapTo(genreDTO, Genre.class));
+        return beanMappingService.mapTo(updated, GenreDTO.class);
+    }
+
+    @Override
+    public void deleteGenre(Long id) {
+        Genre genre = new Genre();
+        genre.setId(id);
+        genreService.deleteGenre(genre);
+    }
+}
