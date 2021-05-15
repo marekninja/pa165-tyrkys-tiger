41adb18ce3bfd6e826c702c829237bff214be831
Marek Petrovič <32146793+marekninja@users.noreply.github.com>
2021-05-15 15:22:00 +0200

    [FIX] quasar structure change - dist not ignored, needed for spring

--- recommender-quasar/quasar.conf.js
+++ recommender-quasar/quasar.conf.js
@@ -72,18 +72,18 @@ module.exports = function( /* ctx */ ) {
         // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
         devServer: {
             https: false,
-            port: 8081,
+            port: 3000,
             open: true, // opens browser window automatically
-            // proxy: {
-            //     // proxy all requests starting with /api to jsonplaceholder
-            //     // '/api': {
-            //     // target: 'http://http://localhost:8080/pa165/api/v1/',
-            //     // changeOrigin: true,
-            //     // pathRewrite: {
-            //     //     '^/api': ''
-            //     //     }
-            //     // }
-            // }
+            proxy: {
+                // proxy all requests starting with /api to jsonplaceholder
+                '/api': {
+                    target: 'http://localhost:8080/pa165/api/v1/',
+                    changeOrigin: true,
+                    pathRewrite: {
+                        '^/api': ''
+                    }
+                }
+            }
         },
 
         // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
