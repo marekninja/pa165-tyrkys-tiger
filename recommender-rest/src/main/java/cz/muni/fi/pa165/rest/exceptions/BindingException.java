5ea785521d1751c6014ca2a14211ce87288ff061
Matej Turek <469107@fi.muni.cz>
2021-05-20 16:00:10 +0200

    [ADD] Exceptions with error response status

---
+++ recommender-rest/src/main/java/cz/muni/fi/pa165/rest/exceptions/BindingException.java
@@ -0,0 +1,15 @@
+package cz.muni.fi.pa165.rest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Matej Turek
+ */
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Error occurred during binding")
+public class BindingException extends RuntimeException {
+
+    public BindingException(String message) {
+        super(message);
+    }
+}
