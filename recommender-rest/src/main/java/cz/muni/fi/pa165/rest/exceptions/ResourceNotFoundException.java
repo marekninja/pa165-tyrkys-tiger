ffc4cc4acb440f6fc6c04080db7ae688e1d0b586
Matej Turek <469107@fi.muni.cz>
2021-05-20 16:00:28 +0200

    [ADD] Exceptions with error response status

--- recommender-rest/src/main/java/cz/muni/fi/pa165/rest/exceptions/ResourceNotFoundException.java
+++ recommender-rest/src/main/java/cz/muni/fi/pa165/rest/exceptions/ResourceNotFoundException.java
@@ -1,11 +1,23 @@
 package cz.muni.fi.pa165.rest.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Exception converted by MyExceptionHandler to NOT_FOUND HTTP status.
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "the resource was not found!")
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
         super(message);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public ResourceNotFoundException( String message, Throwable cause) {
+        super(message, cause);
     }
 }
