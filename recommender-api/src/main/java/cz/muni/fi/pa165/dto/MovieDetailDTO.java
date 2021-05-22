e32148d1833301583b6128297bae40f9b81b1369
Marek Petrovič <32146793+marekninja@users.noreply.github.com>
2021-05-22 11:59:42 +0200

    [ADD] constraints for movie related DTOs

--- recommender-api/src/main/java/cz/muni/fi/pa165/dto/MovieDetailDTO.java
+++ recommender-api/src/main/java/cz/muni/fi/pa165/dto/MovieDetailDTO.java
@@ -3,6 +3,7 @@ package cz.muni.fi.pa165.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
@@ -16,7 +17,7 @@ import java.util.Set;
 @Setter
 public class MovieDetailDTO {
 
-
+    @NotNull
     private Long id;
 
     private String name;
