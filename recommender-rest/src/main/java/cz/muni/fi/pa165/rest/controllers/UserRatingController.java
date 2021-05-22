fecb245b0ec27804496507d22aa29c6f09231a6f
Matej Turek <469107@fi.muni.cz>
2021-05-23 00:27:52 +0200

    [ADD] Method for findById

--- recommender-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/UserRatingController.java
+++ recommender-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/UserRatingController.java
@@ -12,6 +12,7 @@ import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.HttpStatus;
@@ -21,12 +22,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 /**
  * @author Matej Turek
  */
 @RestController
-@ExposesResourceFor(UserRatingViewDTO.class)
+@ExposesResourceFor(UserRatingDTO.class)
 @RequestMapping(Uris.ROOT_URI_RATINGS)
 public class UserRatingController {
 
@@ -43,18 +47,45 @@ public class UserRatingController {
     }
 
     /**
+     * Handles GET request to find user rating by specific id.
+     *
+     * @return ResponseEntity with UserRatingDTO object in json representation and status report
+     */
+    @ApiOperation(value = "Find user rating with given id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = ResponseEntity.class),
+            @ApiResponse(code = 404, message = "Not Found")
+    })
+    @GetMapping(value = "/{id}",
+            produces = "application/hal+json")
+    public ResponseEntity<EntityModel<UserRatingDTO>> findUserRatingById(@PathVariable Long id) throws ResourceNotFoundException {
+        logger.debug("rest findUserRatingById() - get user rating by id.");
+
+        UserRatingDTO userRating = userRatingFacade.findUserRatingById(id);
+
+        if (userRating == null) {
+            throw new ResourceNotFoundException("User rating with id = {" + id + "} not found.");
+        }
+
+        EntityModel<UserRatingDTO> entityModel = userRatingRepresentationalModelAssembler.toModel(userRating);
+
+        return new ResponseEntity<>(entityModel, HttpStatus.OK);
+    }
+
+    /**
      * Handles POST request to create new genre
      *
      * @return ResponseEntity with UserRatingDTO object in json representation and status report
      */
     @ApiOperation(value = "User registration")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK", response = ResponseEntity.class)
+            @ApiResponse(code = 201, message = "Created", response = ResponseEntity.class),
+            @ApiResponse(code = 500, message = "Internal Server Error")
     })
     @PostMapping(value = "/create",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = "application/hal+json")
-    public ResponseEntity<EntityModel<UserRatingViewDTO>> createUserRating(@RequestBody @Valid UserRatingDTO userRatingDTO, BindingResult bindingResult) {
+    public ResponseEntity<EntityModel<UserRatingDTO>> createUserRating(@RequestBody @Valid UserRatingCreateDTO userRatingCreateDTO, BindingResult bindingResult) throws CouldNotCreateException {
         logger.debug("rest createUserRating() - authenticate the user");
 
         if (bindingResult.hasErrors()) {
@@ -62,10 +93,15 @@ public class UserRatingController {
             throw new BindingException("Error occurred during binding");
         }
 
-        UserRatingViewDTO newUserRating = userRatingFacade.createUserRating(userRatingDTO);
-        EntityModel<UserRatingViewDTO> entityModel = userRatingRepresentationalModelAssembler.toModel(newUserRating);
+        try {
+            UserRatingDTO newUserRating = userRatingFacade.createUserRating(userRatingCreateDTO);
+            EntityModel<UserRatingDTO> entityModel = userRatingRepresentationalModelAssembler.toModel(newUserRating);
 
-        return new ResponseEntity<>(entityModel, HttpStatus.CREATED);
+            return new ResponseEntity<>(entityModel, HttpStatus.CREATED);
+        } catch (NullArgumentException ex) {
+            logger.error("Error has occurred during creating.\nReason: {}", ex.getMessage());
+            throw new CouldNotCreateException("Error has occurred during creating.", ex);
+        }
     }
 
     /**
@@ -82,7 +118,7 @@ public class UserRatingController {
     @PutMapping(value = "/update",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = "application/hal+json")
-    public final ResponseEntity<EntityModel<UserRatingViewDTO>> updateUserRating(@RequestBody @Valid UserRatingDTO userRatingDTO, BindingResult bindingResult) {
+    public final ResponseEntity<EntityModel<UserRatingDTO>> updateUserRating(@RequestBody @Valid UserRatingDTO userRatingDTO, BindingResult bindingResult) {
         logger.debug("rest updateUserRating() - update the user rating");
 
         if (bindingResult.hasErrors()) {
@@ -91,12 +127,12 @@ public class UserRatingController {
         }
 
         try {
-            UserRatingViewDTO updatedUserRating = userRatingFacade.updateUserRating(userRatingDTO);
-            EntityModel<UserRatingViewDTO> entityModel = userRatingRepresentationalModelAssembler.toModel(updatedUserRating);
+            UserRatingDTO updatedUserRating = userRatingFacade.updateUserRating(userRatingDTO);
+            EntityModel<UserRatingDTO> entityModel = userRatingRepresentationalModelAssembler.toModel(updatedUserRating);
 
             return new ResponseEntity<>(entityModel, HttpStatus.OK);
         } catch (Exception ex) {
-            logger.error("Error has occurred during update.");
+            logger.error("Error has occurred during update.\nReason: {}", ex.getMessage());
             throw new CouldNotUpdateException("Error occurred during update.", ex);
         }
     }
@@ -119,11 +155,11 @@ public class UserRatingController {
 
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (NullArgumentException ex) {
-            logger.error("User rating with id: {} is not in db.", id);
+            logger.error("User rating with id: {} is not in db.\nReason: {}", id, ex.getMessage());
             throw new ResourceNotFoundException(String.format("User rating with id: {%d} is not in db.", id), ex);
         } catch (IllegalArgumentException ex) {
-            logger.error("Error has occurred during deleting user rating with id: {}", id);
-            throw new IllegalArgumentException(ex);
+            logger.error("Error has occurred during deleting user rating with id: {}.\nReason: {}", id, ex.getMessage());
+            throw new IllegalArgumentException(String.format("Error has occurred during deleting user rating with id: {%d}", id), ex);
         }
     }
 }
