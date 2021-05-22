9505c72f93f97c1d30a0756fcdfa7b32abc3a681
Matej Turek <469107@fi.muni.cz>
2021-05-23 01:14:56 +0200

    [ADD] Added response status

--- recommender-service/src/main/java/cz/muni/fi/pa165/service/exceptions/NullArgumentException.java
+++ recommender-service/src/main/java/cz/muni/fi/pa165/service/exceptions/NullArgumentException.java
@@ -1,8 +1,12 @@
 package cz.muni.fi.pa165.service.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author Matej Turek
  */
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Input parameter was null.")
 public class NullArgumentException extends IllegalArgumentException {
     public NullArgumentException(Class<?> clazz, String message) {
         super(String.format("{%s - %s}", clazz.getSimpleName(), message));
